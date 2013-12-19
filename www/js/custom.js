9811addf713a2eaa471509345b9744c9d20c5ebe
faizan <faizan@gems.techverx.com>
2013-12-19 20:33:58 +0500

    pre final test

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,7 +1,50 @@
 // general vars
 var tapEvent = 'click'; // tap event click
 
+  jQuery(document).ready(function($) {
 
+                 alert("sdfsf");
+                 $.ajax({
+                    type: 'GET',
+                    url: "http://test3.uscommunities.org/index.php?id=729&type=1383156157",
+                    dataType: 'json',
+                    success: function(data)
+                    {
+                        var items = [];
+                        for (i in data)
+                        {
+                            items.push("<li class='link' id='" + i + "' href='" + data[i].url + "'>" + data[i].title + "</li>");
+                            if (data[i].subpages.length != 0)
+                            {
+
+                                for (j in data[i].subpages)
+                                {
+                                    items.push("<li class='link' style='margin-left:3%;' id='" + i + j + "' href='" + data[i].subpages[j].url + "'>- " + data[i].subpages[j].title + "</li>");
+                                }
+
+                            }
+
+                        }
+                        $(".dropBox").html(items.join(""));
+                            $(".link").click(function() {
+                            var href = $(this).attr("href") + "?tx_bnadaptiveprofile=Phone"; 
+                            $.ajax({
+                                type: 'GET',
+                                url: href,
+                                dataType: 'html',
+                                success: function(data)
+                                {
+                                    $("#main").html(data);
+                                }
+                                
+                            });
+                        });
+    
+                    }
+            }
+                );
+                
+            });
 $.ui.ready(function(){
 	// Navigating pages without services
 	$('.log').bind(tapEvent,function(){
