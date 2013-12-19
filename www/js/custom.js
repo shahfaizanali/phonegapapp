ea996d98df8dd0baef72f4ba1f6a745c096b2369
faizan <faizan@gems.techverx.com>
2013-12-19 23:01:05 +0500

    ui changes

--- www/js/custom.js
+++ www/js/custom.js
@@ -12,26 +12,27 @@ var tapEvent = 'click'; // tap event click
                         var items = [];
                         for (i in data)
                         {
-                            items.push("<li  id='" + i + "' href='" + data[i].url + "'>" + data[i].title + "</li>");
+                            items.push("<li  id='" + i + "' href='" + data[i].url + "'>" +"<a href='##'>"+ data[i].title +"<span></span></a>" + "</li>");
                             if (data[i].subpages.length != 0)
                             {
                                 for (j in data[i].subpages)
                                 {
-                                    items.push("<li  id='" + i + j + "' href='" + data[i].subpages[j].url + "'>- " + data[i].subpages[j].title + "</li>");
+                                    items.push("<li  id='" + i + j + "' href='" + data[i].subpages[j].url + "'>- " + "<a href='##'>"+ data[i].subpages[j].title +"<span></span></a>" + "</li>");
                                 }
                             }
 
                         }
                         $(".dropBox").html(items.join(""));
-                            $(".dropBox li").click(function() {
-                            var href = $(this).attr("href") + "?tx_bnadaptiveprofile=Phone"; 
+                            $(".dropBox li a").click(function() {
+                            var href = $(this).parent().attr("href") + "?tx_bnadaptiveprofile=Phone"; 
                             $.ajax({
                                 type: 'GET',
                                 url: href,
                                 dataType: 'html',
                                 success: function(data)
                                 {
-                                    $("#main").html(data);
+                                    $("#content").html(data);
+                                    $('popupButton').click();
                                 }
                                 
                             });
