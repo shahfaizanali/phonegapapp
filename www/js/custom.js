885b68e885b1f92cdffa98d8021a109a1059f1a6
faizan <faizan@gems.techverx.com>
2013-12-20 23:35:54 +0500

    cdsc

--- www/js/custom.js
+++ www/js/custom.js
@@ -25,8 +25,8 @@ var tapEvent = 'click'; // tap event click
                         $(".dropBox").html(items.join(""));
                             $(".dropBox li a").click(function() {
                             var href = $(this).parent().attr("href") + "?tx_bnadaptiveprofile=Phone";
-//			    $("#content").load(href,function(){$('popupButton').click();});
-			    $("#content").html(href);
+			    $("#content").load(href,function(){$('popupButton').click();});
+//			    $("#content").html(href);
                            
                             $('popupButton').click();
                         });
