165b35e3f54fc52564d6281e3716525cf4489be5
faizan <faizan@gems.techverx.com>
2013-12-20 19:00:23 +0500

    sfjak

--- www/js/custom.js
+++ www/js/custom.js
@@ -26,7 +26,7 @@ var tapEvent = 'click'; // tap event click
                             $(".dropBox li a").click(function() {
                             var href = $(this).parent().attr("href") + "?tx_bnadaptiveprofile=Phone";
 //			    $("#content").load(href,function(){$('popupButton').click();});
-			    $("#main").html('<object width="100" height="100" data="'+href+'">');
+			    $("#content").html('<object width="120" height="400" data="'+href+'">');
                            
                             $('popupButton').click();
                         });
