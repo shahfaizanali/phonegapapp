e733f8eaae0ffca9f9dc96d6a05b55bcbb5f1c00
faizan <faizan@gems.techverx.com>
2013-12-20 21:12:10 +0500

    new html

--- www/js/custom.js
+++ www/js/custom.js
@@ -1,6 +1,12 @@
 // general vars
 var tapEvent = 'click'; // tap event click
-
+$.ui.ready(function(){
+	// Navigating pages without services
+	$('.popupButton').bind(tapEvent,function(){
+		$.ui.loadContent('sliderScreen',false,false,'slide');
+	});
+	
+});
   jQuery(document).ready(function($) {
       
                 $.ajax({
@@ -10,23 +16,36 @@ var tapEvent = 'click'; // tap event click
                     success: function(data)
                     {
                         var items = [];
+                        
                         for (i in data)
                         {
-                            items.push("<li  id='" + i + "' href='" + data[i].url + "'>" +"<a href='##'>"+ data[i].title +"<span></span></a>" + "</li>");
+                            var subitems = [];
                             if (data[i].subpages.length != 0)
                             {
+                                items.push("<li  id='" + i + "' href='" + data[i].url + "'>" +"<a href='##'>"+ data[i].title +"</a><a href='#sliderScreen"+i+"' class='arrow'></a></li>");
                                 for (j in data[i].subpages)
                                 {
-                                    items.push("<li  id='" + i + j + "' href='" + data[i].subpages[j].url + "'>- " + "<a href='##'>"+ data[i].subpages[j].title +"<span></span></a>" + "</li>");
+                                    subitems.push("<li  id='" + i + j + "' href='" + data[i].subpages[j].url + "'>" + "<a href='##'>"+ data[i].subpages[j].title +"</a></li>");
                                 }
+                                $( "<div/>", { scrolling:"yes", "data-footer":"none","data-header":"customHeader",
+                                    "class": "panel",id:"sliderScreen"+i,
+                                    html: "<div class='slider'><br class='all'></div>"}).appendTo( "#content" );
+                                $( "<ul/>", {class: "dropBox",html: subitems.join( "" )}).appendTo( "#sliderScreen"+i+" .slider");
+                                
+                                
+                                
                             }
+                            else
+                               items.push("<li  id='" + i + "' href='" + data[i].url + "'>" +"<a href='##'>"+ data[i].title +"</a></li>");
+
 
                         }
-                        $(".dropBox").html(items.join(""));
+                        items.push(" <br clear='all'>");
+                        $("#sliderScreen .dropBox").html(items.join(""));
                             $(".dropBox li a").click(function() {
                             var href = $(this).parent().attr("href") + "?tx_bnadaptiveprofile=Phone";
 //			    $("#content").load(href,function(){$('popupButton').click();});
-			    $("#content").html('<object style="width:100%; height:100%;" data="'+href+'">');
+			    $("#main").html('<object style="width:100%; height:100%;" data="'+href+'">');
                            
                             $('popupButton').click();
                         });
